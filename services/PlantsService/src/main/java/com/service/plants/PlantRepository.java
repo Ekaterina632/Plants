211220a80789be8324f04bfe63ebeae16f50d739
Ekaterina <matveeva.ekaterina632@gmail.com>
2024-11-25 20:07:33 +0100

    CreatePlantRequest added

--- services/PlantsService/src/main/java/com/service/plants/PlantRepository.java
+++ services/PlantsService/src/main/java/com/service/plants/PlantRepository.java
@@ -36,4 +36,26 @@ public class PlantRepository {
         }
         return null;  // Return null if no plant found with the given ID
     }
+
+    public int savePlant(String name) {
+        String sql = "INSERT INTO Plants (PlantName) VALUES (?)";
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, name);
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows > 0) {
+                // Get the generated PlantID (auto-incremented)
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        return generatedKeys.getInt(1);  // Return the generated ID
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;  // Return -1 if something goes wrong
+    }
 }
